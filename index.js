147545f7a3035ce900aabe06a999ceffccc63180
Saloni <subedisaloni2002@gmail.com>
2021-01-01 20:15:07 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+var randomNumber1 = Math.floor(Math.random()*6) + 1;
+
+var randomImage1 = "images/dice" + randomNumber1 + ".png";
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImage1);
+
+
+var randomNumber2 = Math.floor(Math.random()*6) + 1;
+
+var randomImage2 = "images/dice" + randomNumber2 + ".png";
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src", randomImage2);
+
+if(randomNumber1>randomNumber2)
+{
+    document.querySelector("h1").innerHTML="Player 1 Wins.🚩"
+}
+
+else if(randomNumber2>randomNumber1)
+{
+    document.querySelector("h1").innerHTML="Player 2 Wins.🚩"
+}
+else
+{
+    document.querySelector("h1").innerHTML="It's a Draw."
+}
+
